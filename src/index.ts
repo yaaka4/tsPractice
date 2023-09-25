ff2604d0b9c1855ef7a8fc4769d64cdb7c5d14cb
yuki.furusawa <hrk.yszwz@gmail.com>
2023-09-26 02:12:02 +0900

    json sample

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
 const message: string = "Hello, World";
 
 console.log(message);
+
+const jsonSample: string = `{
+    "games": [
+      {
+        "title": "Title1",
+        "genres": ["ACT"]
+      },
+      {
+        "title": "Title2",
+        "genres": ["ACT", "RPG"]
+      },
+      {
+        "title": "Title3",
+        "genres": ["STG"],
+        "note": "Fantastic shooting game"
+      }
+    ]
+  }
+  `;
+
+const parseObj = JSON.parse(jsonSample);
+for (const game of parseObj.games) {
+    const resultRow = [game.title, game.genres];
+    console.log(resultRow.join(','));
+}
+
+
+
